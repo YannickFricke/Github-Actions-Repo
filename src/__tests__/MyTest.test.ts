e97d9fd157983ba3883772839d49582407d522c1
Yannick Fricke <yannickfricke@googlemail.com>
2021-03-05 23:24:02 +0100

    Tests entfernt

--- src/__tests__/MyTest.test.ts
+++ src/__tests__/MyTest.test.ts
@@ -1,6 +1,5 @@
 describe('Meine Tests', () => {
     it('Mein erster Test', () => {
         expect(true).toBe(true);
-        expect(process.env.MEINE_UMGEBUNGS_VARIABLE).toEqual('TEST123');
     });
 });
