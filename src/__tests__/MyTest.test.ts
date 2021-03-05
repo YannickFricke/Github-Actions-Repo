c228165156a71dbfca3d4a382bb43ec560436c31
Yannick Fricke <yannickfricke@googlemail.com>
2021-03-05 21:46:42 +0100

    Testen von Secrets

--- src/__tests__/MyTest.test.ts
+++ src/__tests__/MyTest.test.ts
@@ -1,5 +1,6 @@
 describe('Meine Tests', () => {
     it('Mein erster Test', () => {
         expect(true).toBe(true);
+        expect(process.env.MEINE_UMGEBUNGS_VARIABLE).toEqual('TEST123');
     });
 });
